8774fb7526ef02241c341cb06a7f9ad95d13dd5e
Jackson Mwongera Irura <irura.mwongera11@gmail.com>
2025-06-01 21:56:24 +0300

    Add weather advice script and handle deletions

--- python_introduction/rectangle_area.py
+++
@@ -1,9 +0,0 @@
-# Defining variables
-length = 10
-width = 5
-
-# Performing area of a rectangle
-area = length * width
-
-# Printing area of the rectangle
-print("The area of the rectangle is:", area)

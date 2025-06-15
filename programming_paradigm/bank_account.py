cd67d1760c5ab3aaed08bb0c1decdf9d7e8ca157
Jackson Mwongera Irura <irura.mwongera11@gmail.com>
2025-06-15 11:29:52 +0300

    bank_account.py

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -1,22 +1,16 @@
-# bank_account.py
-
 class BankAccount:
     def __init__(self, initial_balance=0):
-        self.__account_balance = initial_balance  # Encapsulated attribute
+        self.__account_balance = initial_balance
 
     def deposit(self, amount):
-        if amount > 0:
-            self.__account_balance += amount
-        else:
-            print("Deposit amount must be positive.")
+        self.__account_balance += amount
 
     def withdraw(self, amount):
         if amount <= self.__account_balance:
             self.__account_balance -= amount
             return True
         else:
-            print("Insufficient funds.")
             return False
 
     def display_balance(self):
-        print(f"Current balance: KES {self.__account_balance:.2f}")
+        print(f"Current balance: {self.__account_balance}")
